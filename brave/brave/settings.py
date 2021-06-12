818ac1cc69d712084bfcc22907a55044428e6963
Omiete Briggs <56251240+omi-cpu@users.noreply.github.com>
2021-06-12 17:17:43 +0100

    Update settings.py

--- brave/brave/settings.py
+++ brave/brave/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dd=y$axb4d_m$b)@hvl&8md2$$#b6d-&kw1d(x(##qlujll+5n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
